16a7311d755e269b300aa0e418d73fa52279952f
jaosorio1013 <jaosorio1013@gmail.com>
2015-01-10 11:23:50 -0500

    Configuracion basica

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,9 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+		# Vendor
+		'Way\Generators\GeneratorsServiceProvider',
+		'Barryvdh\Debugbar\ServiceProvider',
 	),
 
 	/*
