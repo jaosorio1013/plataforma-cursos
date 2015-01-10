157820086cda99f4c1a5a0548da634bd58d86c2a
jaosorio1013 <jaosorio1013@gmail.com>
2015-01-10 12:36:51 -0500

    Organizando las migraciones

---
+++ app/PlataformaCursos/Entities/RespuestaPregunta.php
@@ -0,0 +1,10 @@
+<?php namespace PlataformaCursos\Entities;
+
+
+class RespuestaPregunta extends \Eloquent {
+
+    protected $table = 'respuesta_preguntas';
+    protected $fillable = [];
+    protected $perPage = 8;
+
+}
---
+++ app/PlataformaCursos/Entities/TipoReto.php
@@ -0,0 +1,10 @@
+<?php namespace PlataformaCursos\Entities;
+
+
+class TipoReto extends \Eloquent {
+
+    protected $table = 'tipo_retos';
+    protected $fillable = [];
+    protected $perPage = 8;
+
+}
